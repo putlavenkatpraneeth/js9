697d59e82786753221980e137ec32541e2c3854f
putla venkat praneeth <putlavenkat123@gmail.com>
2024-10-28 11:22:25 +0530

    change

---
+++ inde.js
@@ -0,0 +1,28 @@
+var ip=[3,5,10,15,23,22,25,26];
+var op=[]
+for(i in ip){
+    if(ip[i]%5==0)
+        op.push(ip[i])
+       // console.log(ip[i]);
+}
+console.log(op);
+
+
+var arr=["htmL","csS","javascrIPt","react","angulaR","nodE","EXPResS"];
+var arr1=[]
+for(i in arr){
+    if(arr[i].at(-1)==arr[i].at(-1).toUpperCase())
+        arr1.push(arr[i])
+        //console.log(arr[i])
+}
+console.log(arr1);
+
+
+var arr=["html","css","react","js","angular"];
+var arr2=[]
+for(i in arr){
+    if(i%2==0)
+        //console.log(arr[i].toUpperCase())
+    arr2.push(arr[i].toUpperCase())
+}
+console.log(arr2);
